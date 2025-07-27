9302784d60c2007f1119d58deb6d4729316315ef
Mayowa Oladimeji <dimejikassie@gmail.com>
2025-07-27 21:02:53 +0100

    lazy loading

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,24 +1,29 @@
 import type { Route } from "./+types/home";
+import { lazy, Suspense } from "react";
 
 import MyNavbar from "~/pages/navbar/navbar";
 import About from "~/pages/about/about";
 import Portal from "~/pages/portal/portal";
-import CharacterShow from "~/pages/slideshow/characterShow";
+
 import MyFooter from "~/pages/footer/footer";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Multiverse Mayhem" },
-    { name: "description", content: "Welcome to React Router!" },
+    { name: "description", content: "Rick and Morty!" },
   ];
 }
+const CharacterShow = lazy(() => import("~/pages/slideshow/characterShow"));
 
 export default function Home() {
   return (
     <>
       <MyNavbar />
       <About />
-      <CharacterShow />
+      <Suspense fallback={<div>Loading characters...</div>}>
+        <CharacterShow />
+      </Suspense>
+
       <Portal />
       <MyFooter />
     </>
